565925b7d79d223cfbf87e659423a45a9578052b
Nick Wong <nicolaswong01@gmail.com>
2022-06-02 14:00:58 -0700

    Add grid creation based on user input

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+const CONTAINER_SIZE = 800;
+
 function makeGrid() {
-    const grid_size = document.getElementById("grid-size").value;
-    if (grid_size == "default") {
+    // get form submission
+    const num_blocks = document.getElementById('grid-size').value;
+    if (num_blocks == "default") {
         alert("Please select a grid size to continue.");
+        return;
+    }
+    // create grid
+    const container = document.querySelector('.container');
+    container.style['grid-template-columns'] = `repeat(${num_blocks}, ${CONTAINER_SIZE/num_blocks}px)`
+    for (let i = 0; i < Math.pow(num_blocks, 2); i++) {
+        const block = document.createElement('div');
+        block.style.border = '1px solid black';
+        container.appendChild(block);
     }
 }
