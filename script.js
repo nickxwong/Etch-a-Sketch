f53fb2ef2cbc80461d731382332dc4dd3ea0325c
Nick Wong <nicolaswong01@gmail.com>
2022-06-02 13:01:08 -0700

    Add function to retrieve grid size from user

---
+++ script.js
@@ -0,0 +1,6 @@
+function makeGrid() {
+    const grid_size = document.getElementById("grid-size").value;
+    if (grid_size == "default") {
+        alert("Please select a grid size to continue.");
+    }
+}
