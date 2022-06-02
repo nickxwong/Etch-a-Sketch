c9226d844f2fe0a5dff3fb717d1f03e35b39e12b
Nick Wong <nicolaswong01@gmail.com>
2022-06-02 14:16:35 -0700

    Add a clearGrid() function for use by the reset button and makeGrid()

--- script.js
+++ script.js
@@ -7,17 +7,26 @@ function makeGrid() {
         alert("Please select a grid size to continue.");
         return;
     }
+    // clear grid if needed
+    clearGrid();
     // create grid
     const container = document.querySelector('.container');
     container.style['grid-template-columns'] = `repeat(${num_blocks}, ${CONTAINER_SIZE/num_blocks}px)`
     for (let i = 0; i < Math.pow(num_blocks, 2); i++) {
         const block = document.createElement('div');
+        block.classList.add('block');
         block.style.border = '1px solid black';
         container.appendChild(block);
     }
 }
 
+function clearGrid() {
+    const container = document.querySelector('.container');
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach(block => container.removeChild(block));
+}
+
 const reset = document.querySelector('#reset');
 reset.addEventListener('click', () => {
-    location.reload();
+    clearGrid();
 });
